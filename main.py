2373cae90e9286cbd159897c135282a2bd3eda34
Administrator <h13983092340@163.com>
2021-10-01 20:55:27 +0800

    首次提交

---
+++ main.py
@@ -0,0 +1,20 @@
+import xlrd
+import json
+
+data = xlrd.open_workbook("Book1.xlsx")
+table = data.sheets()[0]
+n = table.nrows
+stu = []
+for i in range(n):
+    if i == 0:
+        continue
+    stu.append(table.row_values(i))
+json_data = json.dumps(stu)
+with open('stu_json.json','w') as f :
+    f.write(json_data)
+
+with open('stu_json.json','r') as f:
+    d = json.load(f)
+    print(d)
+    r = list(filter(lambda j: j[2] > 5, d))
+    print("亲密度大于5的人有:",r)
